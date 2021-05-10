a9aca01e30d231546e8c44a591e549acaf688e75
ayeven <dihi.mel@gmail.com>
2021-05-10 11:20:14 +0000

    new folders and files

--- recipes_merge.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const a1 = require('./bdocodex/alch_recipes.json');
-const a2 = require('./bdocodex/cook_recipes.json')
-const merge = a1.concat(a2);
-fs.writeFileSync('./bdocodex/complete_ids.json',JSON.stringify(merge));
